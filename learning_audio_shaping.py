943a05a3f3bd9fceb1444266f8272f55d72f6817
23CH3COOH <kurimannjuu2_ybb_ne_jp@yahoo.co.jp>
2023-08-14 13:36:13 +0900

    ピッチ分布の解析スクリプトを作成

--- learning_audio_shaping.py
+++ learning_audio_shaping.py
@@ -6,17 +6,23 @@ from wav_io import input_wav_file, output_wav_file
 from edge_fading import fade_edge
 from silence import change_length_edge_silence
 
-
+# [Hack] length（長さ）だと単位が分からないのでmsec（ミリ秒）のほうがよさそう
 class Settings:
     def __init__(self):
         self.amp_rate = 1.0  # リサンプリング時の波形倍率（オーバーフロー対策）
         self.length_fade_in = 50.0  # フェードインさせる長さ[msec]
         self.length_fade_out = 50.0  # フェードアウトさせる長さ[msec]
-        self.length_start_silence = 50.0  # 開始の無音の長さ[msec]
-        self.length_end_silence = 50.0  # 終了の無音の長さ[msec]
+        self.length_start_silence = 10.0  # 開始の無音の長さ[msec]
+        self.length_end_silence = 10.0  # 終了の無音の長さ[msec]
         self.output_samling_rate = 40000  # 出力wavのサンプリングレート[Hz]
 
 
+'''
+発話分割した学習用wavファイルを以下の手順でRVC用に整形する
+1. サンプリング周波数を変える（RVC-betaのv2は40000Hzのみ対応なので）
+2. 開始部分をフェードインさせ終了部分をフェードアウトさせる
+3. 開始部分と終了部分の無音の長さを整える
+'''
 def shape_wavs(input_dir_path, settings, output_dir_path):
     for file_name in os.listdir(input_dir_path):
         if not '.wav' in file_name:
@@ -33,6 +39,8 @@ def shape_wavs(input_dir_path, settings, output_dir_path):
         osr = settings.output_samling_rate
 
         if not isr == osr:
+            if wav.dtype == np.int16 and amp * np.max(np.abs(wav)) >= 32400:
+                print('Warning: Overflow possibility in resampling.')
             wav_f = amp * wav.astype(np.float64)
             wav = resample(wav_f, orig_sr=isr, target_sr=osr).astype(wav.dtype)
         fade_edge(osr, lfi, lfo, wav)
---
+++ pitch_distribution.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pyworld
+from wav_io import input_wav_file
+
+
+class MusicalScaleDivider:
+    def __init__(self):
+        self.__min_steps_from_A4 = -17
+        self.__step_size = 30
+        self.__scales = ['E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+                         'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4',
+                         'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5',
+                         'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5']
+        assert len(self.__scales) == self.__step_size
+        self.__seconds = [0.0 for i in range(self.__step_size)]
+
+    def add_pitch(self, pitch, seconds_per_frame=0.005):
+        for v in pitch:
+            if v < 0.0001 or np.isnan(v):
+                continue
+            steps_from_A4 = 12 * np.log2(v / 440)
+            index = int((-1) * self.__min_steps_from_A4 + 0.5 + steps_from_A4)
+            if index < 0:
+                index = 0
+            if index >= self.__step_size:
+                index = self.__step_size - 1
+            self.__seconds[index] += seconds_per_frame
+
+    def get_scales(self):
+        return self.__scales
+
+    def get_seconds(self):
+        return self.__seconds
+
+
+def output_graph(scales, seconds, dir_path, adding_label):
+    size = len(scales)
+    assert len(seconds) == size
+
+    plt.figure(figsize=(19.2, 10.8)) 
+    plt.xlabel('pitch')
+    plt.ylabel('seconds')
+    title_str = 'Pitch Distribution {}'.format(adding_label)
+    desc_str = 'Total: {:.3f}[sec]'.format(np.sum(np.array(seconds)))
+    plt.title(title_str + '\n' + desc_str)
+
+    plt.bar(np.arange(size), seconds, width=0.8, tick_label=scales)
+    # 保存直前にplt.show()すると保存しても中身が描かれない模様
+    plt.savefig(dir_path + 'pitch_distribution_{}.png'.format(adding_label))
+
+
+def analyze_pitch(wav, sr):
+    wav_f = wav.astype(np.float64)
+    pitch_temp, time = pyworld.dio(wav_f, sr)
+    return pyworld.stonemask(wav_f, pitch_temp, time, sr)
+
+
+def output_pitch_distribution(input_dir_path, output_dir_path, adding_label):
+    msd = MusicalScaleDivider()
+
+    for file_name in os.listdir(input_dir_path):
+        if not '.wav' in file_name:
+            continue
+
+        print('Start analyzing pitch: {}'.format(file_name))
+        sr, wav = input_wav_file(input_dir_path + file_name, to_mono=True)
+        pitch = analyze_pitch(wav, sr)
+        msd.add_pitch(pitch, seconds_per_frame=0.005)
+
+    scales = msd.get_scales()
+    seconds = msd.get_seconds()
+    output_graph(scales, seconds, output_dir_path, adding_label)
+
+
+if __name__ == '__main__':
+    input_dir_path = 'input/'
+    output_dir_path = 'output/'
+    adding_label = 'Test'
+    output_pitch_distribution(input_dir_path, output_dir_path, adding_label)
